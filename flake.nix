1fbb8e8dfcdeb776595d4723e5d9568a5f9e3125
Harsh Shandilya <me@msfjarvis.dev>
2024-11-06 11:39:04 +0530

    fix: upgrade to Rust 1.82.0

--- flake.nix
+++ flake.nix
@@ -39,7 +39,7 @@
 
       rustStable = (import fenix {inherit pkgs;}).fromToolchainFile {
         file = ./rust-toolchain.toml;
-        sha256 = "sha256-6eN/GKzjVSjEhGO9FhWObkRFaE1Jf+uqMSdQnb8lcB4=";
+        sha256 = "sha256-yMuSb5eQPO/bHv+Bcf/US8LVMbf/G/0MSfiPwBhiPpk=";
       };
       graphqlFilter = path: builtins.match ".*graphql$" path != null;
       filter = path: type: (graphqlFilter path) || (craneLib.filterCargoSources path type);
