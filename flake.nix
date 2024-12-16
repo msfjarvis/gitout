5c33676e04960318b0ca0837dd04c619f1c2b41a
Harsh Shandilya <me@msfjarvis.dev>
2024-12-16 18:33:29 +0530

    chore: reformat all files with nixfmt-rfc-style

--- flake.nix
+++ flake.nix
@@ -22,86 +22,100 @@
   inputs.flake-compat.url = "github:nix-community/flake-compat";
   inputs.flake-compat.flake = false;
 
-  outputs = {
-    nixpkgs,
-    advisory-db,
-    crane,
-    devshell,
-    fenix,
-    flake-utils,
-    ...
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [devshell.overlays.default];
-      };
-
-      rustStable = (import fenix {inherit pkgs;}).fromToolchainFile {
-        file = ./rust-toolchain.toml;
-        sha256 = "sha256-yMuSb5eQPO/bHv+Bcf/US8LVMbf/G/0MSfiPwBhiPpk=";
-      };
-      graphqlFilter = path: builtins.match ".*graphql$" path != null;
-      filter = path: type: (graphqlFilter path) || (craneLib.filterCargoSources path type);
-
-      craneLib = (crane.mkLib pkgs).overrideToolchain rustStable;
-      commonArgs = {
-        src = pkgs.lib.cleanSourceWith {
-          src = craneLib.path ./.;
-          inherit filter;
+  outputs =
+    {
+      nixpkgs,
+      advisory-db,
+      crane,
+      devshell,
+      fenix,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ devshell.overlays.default ];
         };
-        buildInputs = [];
-        nativeBuildInputs = with pkgs;[perl pkg-config];
-        cargoClippyExtraArgs = "--all-targets -- --deny warnings";
-      };
-      cargoArtifacts = craneLib.buildDepsOnly (commonArgs // {doCheck = false;});
 
-      gitout = craneLib.buildPackage (commonArgs // {doCheck = false;});
-      gitout-clippy = craneLib.cargoClippy (commonArgs
-        // {
-          inherit cargoArtifacts;
-        });
-      gitout-fmt = craneLib.cargoFmt (commonArgs // {});
-      gitout-audit = craneLib.cargoAudit (commonArgs // {inherit advisory-db;});
-      gitout-nextest = craneLib.cargoNextest (commonArgs
-        // {
-          inherit cargoArtifacts;
-          src = ./.;
-          partitions = 1;
-          partitionType = "count";
-        });
-    in {
-      checks = {
-        inherit gitout;
-        # There are some deprecated dependencies that need to be replaced
-        #inherit gitout-audit;
-        inherit gitout-clippy;
-        inherit gitout-fmt;
-        inherit gitout-nextest;
-      };
+        rustStable = (import fenix { inherit pkgs; }).fromToolchainFile {
+          file = ./rust-toolchain.toml;
+          sha256 = "sha256-yMuSb5eQPO/bHv+Bcf/US8LVMbf/G/0MSfiPwBhiPpk=";
+        };
+        graphqlFilter = path: builtins.match ".*graphql$" path != null;
+        filter = path: type: (graphqlFilter path) || (craneLib.filterCargoSources path type);
 
-      packages.default = gitout;
+        craneLib = (crane.mkLib pkgs).overrideToolchain rustStable;
+        commonArgs = {
+          src = pkgs.lib.cleanSourceWith {
+            src = craneLib.path ./.;
+            inherit filter;
+          };
+          buildInputs = [ ];
+          nativeBuildInputs = with pkgs; [
+            perl
+            pkg-config
+          ];
+          cargoClippyExtraArgs = "--all-targets -- --deny warnings";
+        };
+        cargoArtifacts = craneLib.buildDepsOnly (commonArgs // { doCheck = false; });
 
-      apps.default = flake-utils.lib.mkApp {drv = gitout;};
-
-      devShells.default = pkgs.devshell.mkShell {
-        bash = {interactive = "";};
-
-        env = [
-          {
-            name = "DEVSHELL_NO_MOTD";
-            value = 1;
+        gitout = craneLib.buildPackage (commonArgs // { doCheck = false; });
+        gitout-clippy = craneLib.cargoClippy (
+          commonArgs
+          // {
+            inherit cargoArtifacts;
           }
-        ];
+        );
+        gitout-fmt = craneLib.cargoFmt (commonArgs // { });
+        gitout-audit = craneLib.cargoAudit (commonArgs // { inherit advisory-db; });
+        gitout-nextest = craneLib.cargoNextest (
+          commonArgs
+          // {
+            inherit cargoArtifacts;
+            src = ./.;
+            partitions = 1;
+            partitionType = "count";
+          }
+        );
+      in
+      {
+        checks = {
+          inherit gitout;
+          # There are some deprecated dependencies that need to be replaced
+          #inherit gitout-audit;
+          inherit gitout-clippy;
+          inherit gitout-fmt;
+          inherit gitout-nextest;
+        };
 
-        packages = with pkgs; [
-          cargo-nextest
-          cargo-release
-          gnumake
-          perl
-          rustStable
-          stdenv.cc
-        ];
-      };
-    });
+        packages.default = gitout;
+
+        apps.default = flake-utils.lib.mkApp { drv = gitout; };
+
+        devShells.default = pkgs.devshell.mkShell {
+          bash = {
+            interactive = "";
+          };
+
+          env = [
+            {
+              name = "DEVSHELL_NO_MOTD";
+              value = 1;
+            }
+          ];
+
+          packages = with pkgs; [
+            cargo-nextest
+            cargo-release
+            gnumake
+            perl
+            rustStable
+            stdenv.cc
+          ];
+        };
+      }
+    );
 }
